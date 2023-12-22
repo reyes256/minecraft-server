46ad4da3f58020c25ac6d7d4d81b7453b477783d
Fernando Reyes <fernandoreyes.dev@gmail.com>
2023-12-21 22:44:27 -0700

    Updated container name for bash scripts
    Signed-off-by: Fernando Reyes <fernandoreyes.dev@gmail.com>

--- scripts/deop.sh
+++ scripts/deop.sh
@@ -3,4 +3,4 @@
 # Manda como primer parametro el jugador para quitarle op (./deop.sh ToribioGamer)
 
 [ ! $1 ] && echo "Se necesita al menos 1 jugador" && exit 1
-docker exec minecraft-server mc-send-to-console deop $1
+docker exec mc mc-send-to-console deop $1
--- scripts/op.sh
+++ scripts/op.sh
@@ -3,4 +3,4 @@
 # Manda como primer parametro el jugador para hacerlo op (./op.sh ToribioGamer)
 
 [ ! $1 ] && echo "Se necesita al menos 1 jugador" && exit 1
-docker exec minecraft-server mc-send-to-console op $1
+docker exec mc mc-send-to-console op $1

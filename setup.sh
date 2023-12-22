896aa45e993f43d3d43e35b6ac7bc7f3bf708e77
Fernando Reyes <fernandoreyes.dev@gmail.com>
2023-12-21 22:46:48 -0700

    Updated setup.sh file
    Signed-off-by: Fernando Reyes <fernandoreyes.dev@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 sudo apt update && \
-sudo apt install zip unzip snapd -y
+sudo apt install zip unzip -y
 
 #-------
 # Clone server repo
 cd /opt && \
 sudo git clone https://github.com/reyes256/minecraft-server.git && \
 git config --global --add safe.directory /opt/minecraft-server && \
-sudo chmod -R 777 minecraft-server
+sudo chmod a+w /opt/minecraft-server/data/server.properties
 
 #--------------------------
 #       Set local time
@@ -35,7 +35,7 @@ echo "Cron job appended successfully."
 #       Install docker
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -46,7 +46,7 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 sudo groupadd docker
 sudo usermod -aG docker $USER

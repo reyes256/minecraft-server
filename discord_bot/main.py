f58c2a2d01af8c68b23298174b49c2f764e3d552
Fernando Reyes <fernandoreyes.dev@gmail.com>
2023-12-25 18:20:44 -0700

    Execute commands from discord bot
    Signed-off-by: Fernando Reyes <fernandoreyes.dev@gmail.com>

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -3,13 +3,22 @@ import settings
 import discord
 from discord.ext import commands
 
-def run():
+def run_command(command):
+    try:
+        output = subprocess.check_output(command, shell=True, text=True)
+        return output
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+        return e
+
+def main():
     intents = discord.Intents.default()
     intents.message_content = True
     bot = commands.Bot(command_prefix='!', intents=intents)
     
     @bot.event
     async def on_ready():
+        print('------')
         print(f'Logged in as {bot.user} (ID: {bot.user.id})')
         print('------')
 
@@ -19,6 +28,11 @@ def run():
         
     @bot.command()
     async def say(ctx, *messages):
+        full_message = " ".join(messages)
+        
+        cmd = f"docker exec mc mc-send-to-console say {full_message}"
+        run_command(cmd)
+        
         await ctx.send("Mensaje enviado: " + " ".join(messages))
         
     @bot.command()
@@ -26,32 +40,22 @@ def run():
         if player == "none":
             return
         
-        command = ["docker", "exec", "mc", "mc-send-to-console", "op", player]
-        command2 = ["docker", "compose", "-f","/opt/minecraft-server/docker-compose.yml", "up", "-d"]
-
-        try:
-            result = subprocess.run(command2, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-            print("Command Output:")
-            print(result.stdout)
-
-            if result.stderr:
-                print("Command Error:")
-                print(result.stderr)
-
-            await ctx.send(f"Fakin {player} ya es OP")
-        except subprocess.CalledProcessError as e:
-            # Handle exceptions for non-zero exit codes
-            print(f"Error executing command: {e}")
-            await ctx.send(f"Error ejecutando el comando: {e}")
-        except Exception as e:
-            # Handle other exceptions
-            print(f"An unexpected error occurred: {e}")
-            await ctx.send(f"Error inesperado: {e}")
-
-            
+        cmd = f"docker exec mc mc-send-to-console op {player}"
+        run_command(cmd)
+        
+        await ctx.send(f"Fakin {player} ya es OP")
+    
+    @bot.command()
+    async def deop(ctx, player="none"):
+        if player == "none":
+            return
+        
+        cmd = f"docker exec mc mc-send-to-console deop {player}"
+        run_command(cmd)
+        
+        await ctx.send(f"Mamaste {player}, ya no eres OP")
         
     bot.run(settings.DISCORD_API_TOKEN)
 
 if __name__ == "__main__":
-    run()
+    main()

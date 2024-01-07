c2c5f616d368d35d1201ee05969e2e78aa0c2086
Fernando Reyes <fernandoreyes.dev@gmail.com>
2024-01-07 09:47:59 -0700

    Enforce old backups deletion when making a new backup
    Signed-off-by: Fernando Reyes <fernandoreyes.dev@gmail.com>

--- scripts/backup_data.sh
+++ scripts/backup_data.sh
@@ -13,7 +13,7 @@ zip -v
 cd /opt/minecraft-server/data/ && \
 sudo zip -r ${BACKUP_DIR}/backup-$(date +%Y-%m-%d-%H-%M-%S).zip . 
 
-find "$BACKUP_DIR" -name "backup-*" -mtime +3 -exec bash -c 'echo "Borrando: {}" && rm -f "{}" ' \; # Delete backups older than 3 days
+find "$BACKUP_DIR" -name "backup-*" -mtime +3 -exec bash -c 'echo "Borrando: {}" && sudo rm -f "{}" ' \; # Delete backups older than 3 days
 
 cd /opt/minecraft-server/ && \
 docker compose up -d && \

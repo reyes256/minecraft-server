65a6673e0baaaf1c7e54ec3cb63adb697cb9b0fd
Fernando Reyes <fernandoreyes.dev@gmail.com>
2024-01-25 19:57:52 -0700

    shebang for plugins script

--- scripts/install_plugins.sh
+++ scripts/install_plugins.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo mkdir -p /opt/minecraft-server/plugins
 
 sudo wget -O /opt/minecraft-server/plugins/MCDash-1.1.7.jar https://github.com/gnmyt/MCDash/releases/download/v1.1.7/MCDash-1.1.7.jar

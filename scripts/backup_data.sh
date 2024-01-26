74eee05fab6b2d348fb0ce904911dd6516f3d5b5
Fernando Reyes <fernandoreyes.dev@gmail.com>
2024-01-25 19:55:42 -0700

    Updated backup script
    Signed-off-by: Fernando Reyes <fernandoreyes.dev@gmail.com>

--- scripts/backup_data.sh
+++ scripts/backup_data.sh
@@ -4,16 +4,16 @@ cd /opt/minecraft-server/data/ && \
 docker compose down && \
 echo "Minecraft Server is down"
 
-BACKUP_DIR="/opt/minecraft-server-backups"
+BACKUP_DIR="/opt/world-backups"
 [ ! -d "${BACKUP_DIR}" ] && sudo mkdir ${BACKUP_DIR}
 
 zip -v
 [ $? -ne 0 ] && sudo apt install zip -y
 
 cd /opt/minecraft-server/data/ && \
-sudo zip -r ${BACKUP_DIR}/backup-$(date +%Y-%m-%d-%H-%M-%S).zip . 
+sudo zip -r ${BACKUP_DIR}/backup-$(date +%Y-%m-%d-%H-%M-%S).zip world world_nether world_the_end
 
-find "$BACKUP_DIR" -name "backup-*" -mtime +3 -exec bash -c 'echo "Borrando: {}" && sudo rm -f "{}" ' \; # Delete backups older than 3 days
+find "$BACKUP_DIR" -name "backup-*" -mtime +7 -exec bash -c 'echo "Borrando: {}" && sudo rm -f "{}" ' \; # Delete backups older than a week
 
 cd /opt/minecraft-server/ && \
 docker compose up -d && \

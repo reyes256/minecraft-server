3c1deb0eb47d0e5c631d33b3c7f32f0bbc3e68e1
Fernando Reyes <fernaandoreyes.dev@gmail>
2024-02-03 13:16:05 -0700

    Added python cli to manage mc server
    Signed-off-by: Fernando Reyes <fernaandoreyes.dev@gmail>

---
+++ README.md
@@ -0,0 +1,35 @@
+# Minecraft Server Starter Kit
+
+## Installation
+
+#### Create Virtual Environment
+```sh
+python3 -m venv .venv
+```
+```sh
+source .venv/bin/activate
+```
+
+#### Install Dependencies
+```sh
+pip install --editable .
+```
+
+## Usage
+
+### Server Commands
+
+Start Server
+```sh
+mcli server start
+```
+
+Stop Server
+```sh
+mcli server stop
+```
+
+Restart Server
+```sh
+mcli server restart
+```
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+def read_requirements(file_path):
+    dependencies = []
+    try:
+        with open(file_path, "r") as file:
+            for line in file:
+                line = line.split("#")[0].strip()
+                if line:
+                    dependencies.append(line.strip())
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+    return dependencies
+
+
+requirements_list = read_requirements("requirements.txt")
+setup(
+    name="Minecraft Server CLI",
+    version="0.1.0",
+    py_modules=["cli"],
+    install_requires=requirements_list,
+    entry_points={
+        "console_scripts": [
+            "mcli = cli:mcli",
+        ],
+    },
+)
